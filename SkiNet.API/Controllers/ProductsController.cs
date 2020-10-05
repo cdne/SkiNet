008c6d6426d733c48109d5064f9a5167a111d25e
Claudiu <cduneata@gmail.com>
2020-10-05 11:14:24 +0300

    Add ProductsController.cs

---
+++ SkiNet.API/Controllers/ProductsController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SkiNet.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductsController : ControllerBase
+    {
+        [HttpGet]
+        public string GetProducts()
+        {
+            return "list of products";
+        }
+
+        [HttpGet("{product}")]
+        public string GetProduct(string product)
+        {
+            return $"single product {product}";
+        }
+    }
+}
