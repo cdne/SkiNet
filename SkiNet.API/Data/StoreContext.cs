9cf358e8105ef58d5b8d8c635b4872aacba73053
Claudiu <cduneata@gmail.com>
2020-10-05 11:14:46 +0300

    Add Entity Framework StoreContext.cs

---
+++ SkiNet.API/Data/StoreContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SkiNet.API.Entities;
+
+namespace SkiNet.API.Data
+{
+    public class StoreContext : DbContext
+    {
+        public StoreContext(DbContextOptions<StoreContext> options) : base(options)
+        {
+        }
+        
+        public DbSet<Product> Products { get; set; }
+    }
+}
